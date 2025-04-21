ea8f1289e09dd0ac1bb2bb09ba670ff85c8abd7d
kulraj69 <kulrajs.sabharwal.chy21@itbhu.ac.in>
2025-04-22 03:04:32 +0530

    Renamed fastapi.py to main.py and updated data files

--- backend/main.py
+++ backend/main.py
@@ -1384,4 +1384,4 @@ if __name__ == "__main__":
             port += 1
     
     print(f"Starting server on port {port}")
-    uvicorn.run("sample:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
